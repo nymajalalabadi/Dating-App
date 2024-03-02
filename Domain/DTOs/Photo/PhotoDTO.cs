57d5e96e8230916a5324f37417453822f893a8f2
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-03-02 21:14:48 +0330

    Add New DTOs

---
+++ Domain/DTOs/Photo/PhotoDTO.cs
@@ -0,0 +1,26 @@
+﻿using Domain.DTOs.User;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTOs.Photo
+{
+    public class PhotoDTO
+    {
+        #region Properties
+
+        [Key]
+        public int Id { get; set; }
+
+        public string Url { get; set; }
+
+        public bool IsMain { get; set; }
+
+        #endregion
+
+        public MemberDTO MemberDTO { get; set; }
+    }
+}
---
+++ Domain/DTOs/User/MemberDTO.cs
@@ -0,0 +1,50 @@
+﻿using Domain.DTOs.Photo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTOs.User
+{
+    public class MemberDTO
+    {
+        #region Properties
+
+        [Key]
+        public int UserId { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserName { get; set; }
+
+        public string? Mobile { get; set; }
+
+        public string Avatar { get; set; }
+
+        public bool IsEmailActive { get; set; }
+
+        public int Age { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string LookingFor { get; set; }
+
+        public string Interests { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public string KnowAs { get; set; }
+
+        public DateTime RegisterDate { get; set; }
+
+        #endregion
+
+        public ICollection<PhotoDTO> Photos { get; set; }
+    }
+}
