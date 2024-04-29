5d1cacd509a6130856006077b49dac912b9ea9e5
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-04-29 11:07:05 +0330

    Add User Params For Paging

---
+++ Domain/DTOs/User/UserParams.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTOs.User
+{
+    public class UserParams
+    {
+        private const int maxPageSize = 50;
+
+        public int PageNumber { get; set; } = 1;
+
+        private int _pageSize;
+
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > maxPageSize) ? maxPageSize : value;
+        }
+
+    }
+
+}
